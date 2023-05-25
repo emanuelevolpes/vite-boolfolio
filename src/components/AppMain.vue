3c0d56ddd4546b9866c76103092320112ebd3553
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-25 18:15:48 +0200

    props, card and v-for

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,31 @@
 <template>
     <main>
-        <div>main</div>
+        <div class="container">
+            <div class="row my-4 gy-4">
+                <div class="col col-md-4" v-for="project in data">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
-export default{
-    
+export default {
+    name: 'AppMain',
+    props: {
+        data: Object
+    }
 }
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
