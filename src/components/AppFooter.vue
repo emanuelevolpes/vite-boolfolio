e674659034100883a358c9810a2e6ce7ebdbf654
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-25 17:20:48 +0200

    footer style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,16 +1,31 @@
 <template>
     <footer>
-        <div>footer</div>
+        <ul class="d-flex gap-3">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
     </footer>
 </template>
 
 <script>
-export default{
-    
+export default {
+
 }
 
 </script>
 
 <style lang="scss" scoped>
+footer {
+    background-color: blue;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
+    ul {
+        list-style: none;
+    }
+}
 </style>
