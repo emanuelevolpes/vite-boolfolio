bb197a4e3ab257734d1e3a8c7891ba720049cfbd
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-26 10:46:27 +0200

    Revert "title and content"
    This reverts commit 3a9b7ba97de761d54782481978846ab4752dc52a.

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,7 +9,25 @@ export default {
 
 <template>
     <main>
+<<<<<<< HEAD
         <AppProjectCard :data="projects"/>
+=======
+        <div class="container">
+            <div class="row my-4 gy-4">
+                <div class="col col-md-4" v-for="project in data">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+>>>>>>> parent of 3a9b7ba (title and content)
     </main>
 </template>
 
