bbb5db0300a426784a4bcf205a12933ebf914fdc
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-28 19:42:03 +0200

    project page return always error 404

---
+++ src/pages/ProjectPage.vue
@@ -0,0 +1,51 @@
+<script>
+import axios from 'axios';
+export default {
+    name: "ProjectPage",
+    data() {
+        return {
+            apiBaseUrl: 'http://127.0.0.1:8000/api',
+            apiUrls: {
+                projects: '/projects'
+            },
+            project: null,
+            isError: false,
+            errorMessage: null
+        }
+    },
+    methods: {
+        getProjects() {
+      axios
+        .get(this.apiBaseUrl + this.apiUrls.projects + "/" + this.$route.params.slug)
+        .then((response) => {
+          this.project = response.data.return;
+        }).catch((error) => {
+          console.log(error);
+          this.isError = true;
+          this.errorMessage = error.message;
+        })
+    }
+  },
+  created() {
+    this.getProjects();
+  }
+}
+</script>
+
+
+<template>
+    <section v-if="project">
+        <div class="container">
+            <h1 class="mt-5 mb-3">{{ project.title }}</h1>
+            <p>{{ project.description }}</p>
+        </div>
+    </section>
+    <section v-if="isError">
+        <div class="container">
+            <p>{{ errorMessage }}</p>
+        </div>
+    </section>
+</template>
+
+
+<style scoped></style>
