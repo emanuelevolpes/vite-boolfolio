83ed74311cb9d4e7ae301308d8ae1657e0cdbbf3
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-28 18:53:35 +0200

    about us page

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <h1 class="text-center p-4">ABOUT US</h1>
+    </section>
+</template>
+
+<script>
+    export default{
+        name: 'AboutPage'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
