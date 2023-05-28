689e0a572b580f8308c26680da84eeb4fc9849f5
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-28 19:22:52 +0200

    card project

---
+++ src/components/CardProject.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ project.title }}</h5>
+                <p class="card-text">{{ project.description }}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'CardProject',
+    props: {
+        project: Object
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
