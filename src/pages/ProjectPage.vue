1d784cd974275e2456c97ff4e10172480d0c21b5
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-29 09:13:32 +0200

    project page

--- src/pages/ProjectPage.vue
+++ src/pages/ProjectPage.vue
@@ -1,20 +1,20 @@
 <script>
 import axios from 'axios';
 export default {
-    name: "ProjectPage",
-    data() {
-        return {
-            apiBaseUrl: 'http://127.0.0.1:8000/api',
-            apiUrls: {
-                projects: '/projects'
-            },
-            project: null,
-            isError: false,
-            errorMessage: null
-        }
-    },
-    methods: {
-        getProject() {
+  name: "ProjectPage",
+  data() {
+    return {
+      apiBaseUrl: 'http://127.0.0.1:8000/api',
+      apiUrls: {
+        projects: '/projects'
+      },
+      project: null,
+      isError: false,
+      errorMessage: null
+    }
+  },
+  methods: {
+    getProject() {
       axios
         .get(this.apiBaseUrl + this.apiUrls.projects + "/" + this.$route.params.slug)
         .then((response) => {
@@ -35,17 +35,17 @@ export default {
 
 
 <template>
-    <section v-if="project">
-        <div class="container">
-            <h1 class="mt-5 mb-3">{{ project.title }}</h1>
-            <p>{{ project.description }}</p>
-        </div>
-    </section>
-    <section v-if="isError">
-        <div class="container">
-            <p>{{ errorMessage }}</p>
-        </div>
-    </section>
+  <section v-if="project">
+    <div class="container text-center">
+      <h1 class="mt-5 mb-3">{{ project.title }}</h1>
+      <p>{{ project.description }}</p>
+    </div>
+  </section>
+  <section v-if="isError">
+    <div class="container">
+      <p>{{ errorMessage }}</p>
+    </div>
+  </section>
 </template>
 
 
