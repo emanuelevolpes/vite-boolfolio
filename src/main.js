c99b62a839cae018973cc18736679e3fa1cf4ba1
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-30 15:40:07 +0200

    font-awesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import { router } from './router';
 import * as bootstrap from 'bootstrap';
 import './assets/scss/main.scss';
 
+//font-awesome
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// import { faLinkedin } from '@fortawesome/free-solid-svg-icons';
+
+// library.add(faLinkedin)
+
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
